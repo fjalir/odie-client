c68752bdcb7def42fa0b42b7cb8b467609c562f4
fjalir <christoph.rohmann@googlemail.com>
2017-05-31 01:55:09 +0200

    Improving reporting ui

--- js/vm/documentreporting.js
+++ js/vm/documentreporting.js
@@ -11,7 +11,6 @@ export default class DocumentReporting {
     this.id_or_barcode = null;
     this.reason = null;
     this.status = null;
-    this.errorlabel = null;
     this.submissionEnabled = ko.pureComputed(function(){
       return self.status == null;
     },this);
@@ -19,11 +18,9 @@ export default class DocumentReporting {
     ko.track(this);
     ko.getObservable(this, 'reason').subscribe(function(changes) {
       self.status = null;
-      self.errorlabel = null;
     });
     ko.getObservable(this, 'id_or_barcode').subscribe(function(changes) {
       self.status = null;
-      self.errorlabel = null;
     });
   }
 
@@ -34,14 +31,16 @@ export default class DocumentReporting {
     };
 
     this.status = 'waiting';
-    api.post('document_report', data).done(() => {
-      this.status = 'success';
-    }).error((data) => {
+    api.post('document_report', data, {error: (data) => {
       this.status = 'error';
       let responseJson = JSON.parse(data.responseText);
       let error = responseJson.errors[0];
       if(error == 'document not found')
         this.status = '404';
+      if(error == 'no reason')
+        this.status = '400';
+    }}).done(() => {
+      this.status = 'success';
     });
   }
 }
